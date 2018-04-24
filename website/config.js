18cb89c08efec5209581c01952960e2e7c2c0890
servc4 <31129466+servc4@users.noreply.github.com>
2018-04-24 21:47:55 +0100

    DeuterCoin-pool

---
+++ website/config.js
@@ -0,0 +1,94 @@
+var api = "http://poolhost.com:1817";
+
+var api_blockexplorer = "http://daemonhost.com:18081";
+
+var poolHost = "poolhost.com";
+
+var irc = "irc.freenode.net/#poolhost";
+
+var email = "support@poolhost.com";
+
+var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "http://bce.deutercoin.uk/en/?hash={id}#blockchain_block";
+
+var transactionExplorer = "http://bce.deutercoin.uk/en/?hash={id}#blockchain_transaction";
+
+var themeCss = "themes/default-theme.css";
+
+var networkStat = {
+"dec": [
+        ["pool.deutercoin.uk", "http://pool.deutercoin.uk:8117"],
+        ["pool2.deutercoin.uk", "http://pool2.deutercoin.uk:8117"],
+        ["graftpool.fr/pooldeuter", "http://graftpool.fr:8116"],
+        ["pool.mineallcrypto.com/pool/?symbol=DTC", "http://pool.mineallcrypto.com:5070"]
+    ],
+    "bcn": [
+        ["bcn.mypool.online", "http://bcn.mypool.online:8084"],
+        ["democats.org", "http://pool.democats.org:7603"]
+    ],
+    "bip": [
+        ["bip.mypool.online", "http://bip.mypool.online:18874"],
+        ["democats.org", "http://pool.democats.org:7693"],
+        ["bip.cryptonotepool.com", "http://5.189.135.137:8121"],
+        ["bip.ms-pool.net.ua", "http://bip.ms-pool.net.ua:8117"],
+        ["bip.crypto-coins.club", "http://bip.crypto-coins.club:8118"]
+    ],
+    "coal": [
+        ["coal.mypool.online", "http://coal.mypool.online:7704"],
+        ["democats.org", "http://pool.democats.org:7703"],
+        ["coal.mine2gether.com", "https://coal.mine2gether.com/api"]
+    ],
+    "dsh": [
+        ["dsh.mypool.online", "http://dsh.mypool.online:29084"],
+        ["democats.org", "http://pool.democats.org:7613"]
+    ],
+    "fcn": [
+        ["fcn.mypool.online", "http://fcn.mypool.online:24084"]
+    ],
+    "krb": [
+        ["krb.mypool.online", "http://api.krb.mypool.online:32351"],
+        ["democats.org/pool/?name=karbowanec", "http://pool2.democats.org:7673"],
+        ["pool.karbowanec.com", "http://pool.karbowanec.com:8117"],
+        ["krb.sberex.com", "http://krb.sberex.com:7006"],
+        ["krb.crypto-coins.club", "http://krb.crypto-coins.club:8118"],
+        ["krb.cryptonotepool.com", "http://5.189.135.137:8618"],
+        ["karbowanec.cryptopool.in", "http://karbowanec.cryptopool.in:8117"],
+        ["krb.cryptomine.pro", "http://krb.cryptomine.pro:8117"],
+        ["krb.kopanka.com", "http://krb.kopanka.com:8117"],
+        ["krb.pool.ualinux.com", "http://krb.pool.ualinux.com:8117"],
+        ["krb.mininglamp.ml", "http://krb.mininglamp.ml:8117"],
+        ["krb.multipool.online", "http://krb.multipool.online:8117"],
+        ["krb.tfoto.com.ua", "http://krb.tfoto.com.ua:8117"],
+        ["usa.krb.kopanka.com", "http://usa.krb.kopanka.com:8117"],
+        ["pool.krb-ua.tk", "http://pool.krb-ua.tk:8117"],
+        ["krb.i-holder.net", "http://krb.i-holder.net:8117"],
+        ["krbpool.ml", "http://krbpool.ml:8117"],
+        ["krb.fastpool.io", "http://krb.fastpool.io:8185"],
+        ["krb.ocukminingpool.com", "http://ocukminingpool.com:8118"],
+        ["pool2.karbowanec.com", "http://45.77.55.161:8117"],
+        ["krb.dreampool.info", "https://krb.dreampool.info/api"],
+        ["krb.miner.rocks", "https://krb.miner.rocks/api"],
+        ["easyhash.io/start/krb", "https://api-krb.easyhash.io:443"],
+        ["mine4all.pp.ua", "http://pool2.democats.org:7673"],
+        ["krb-pool.pp.ua", "http://krb-pool.pp.ua:25417"],
+        ["krb.mininggood.com", "http://us2.mininggood.com:8137"],
+        ["krbpool.com", "http://api.krbpool.com:8117"],
+        ["krb.soyminero.es", "http://krb.soyminero.es:8119"],
+        ["karbunkul.ga", "http://172.104.225.140:3389"],
+        ["karbo.coinwire.eu", "http://karbo.coinwire.eu:8118"],
+        ["krb.miningpool.org.ua", "http://165.227.163.153:7118"],
+        ["karbowanec.hashparty.io", "http://karbowanec-pool.hashparty.io:8117"],
+        ["karbo.farm", "http://karbo.farm:8117"],
+        ["karbo.hashvault.pro", "http://karbo.hashvault.pro:8117"]
+    ],
+    "qcn": [
+        ["qcn.mypool.online", "http://qcn.mypool.online:23084"]
+    ],
+    "xci": [
+        ["xci.mypool.online", "http://xci.mypool.online:42004"],
+        ["xci.cryptonotepool.com", "http://5.189.135.137:8119"]
+    ]
+};
